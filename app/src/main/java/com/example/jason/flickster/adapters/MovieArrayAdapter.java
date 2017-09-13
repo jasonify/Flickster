e936d6197d1b1421ec6e0c96111e1048dafe46a0
jason <bjason@gmail.com>
2017-09-12 23:47:39 -0700

    Adding text views and now getting new row view in adapter

--- app/src/main/java/com/example/jason/flickster/adapters/MovieArrayAdapter.java
+++ app/src/main/java/com/example/jason/flickster/adapters/MovieArrayAdapter.java
@@ -1,8 +1,14 @@
 package com.example.jason.flickster.adapters;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
+import com.example.jason.flickster.R;
 import com.example.jason.flickster.models.Movie;
 
 import java.util.List;
@@ -17,4 +23,16 @@ public class MovieArrayAdapter extends ArrayAdapter<Movie> {
      super(context, android.R.layout.simple_list_item_1, movies);
     }
 
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Movie movie = getItem(position);
+
+        if(convertView == null) {
+            LayoutInflater inflater  = LayoutInflater.from(getContext());
+            convertView =  inflater.inflate(R.layout.item_movie, parent, false);
+        }
+        // check if existing view is getting reused
+        return super.getView(position, convertView, parent);
+    }
 }
