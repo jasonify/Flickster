fc1782ea35e49b7690438b3244f6b53908b2e65b
jason <bjason@gmail.com>
2017-09-14 21:05:11 -0700

    Weird scale with images because of fit?

--- app/src/main/java/com/example/jason/flickster/adapters/MovieArrayAdapter.java
+++ app/src/main/java/com/example/jason/flickster/adapters/MovieArrayAdapter.java
@@ -61,11 +61,11 @@ public class MovieArrayAdapter extends ArrayAdapter<Movie> {
         int orientation = getContext().getResources().getConfiguration().orientation;
         if (orientation == Configuration.ORIENTATION_PORTRAIT) {
             Picasso.with(getContext()).load(movie.getPosterPath())
-                    .fit()
+                    .fit().centerCrop()
                     .placeholder(R.drawable.loading).into(viewHolder.movieImage);
         } else if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
             Picasso.with(getContext()).load(movie.getBackgropPath())
-                    .fit()
+                    .fit().centerCrop()
                     .placeholder(R.drawable.loading).into(viewHolder.movieImage);
         }
 
